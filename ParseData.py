ee3977715fa575578422122238d41c55a2ade3a1
blakholesun <robert.maglieri@gmail.com>
2016-01-11 12:26:43 -0500

    Rewrote ParseData

--- ParseData.py
+++ ParseData.py
@@ -1,6 +1,30 @@
 import LoadData as ld
+import pandas as pd
 
-def load():
-	CTdata, RFTdata = ld.loadData()
+def getNumberDays(start, end):
+    # get the number of days not including the weekends
 
-def parse(CTdata,RFTdata)
+def toDataFrame(sqlFilename):
+    dataDict = ld.loadData(sqlFilename)
+    print("Data Loaded")
+    
+    dfDict = []
+
+    for data_name, data in dataDict.items() :
+        tempDat = pd.DataFrame(data)
+        tempDat.columns = map(str.lower, tempDat.coulmns)
+        if data_name == 'RFT' or data_name == 'CT'
+            tempDat.drop_duplicates(subset = 'patientsernum', 
+                                    keep = 'last', inplace = True)
+            tempDat.reset_index(drop = True, inplace = True)
+
+        dfList = dfList.append(tempDat)
+
+
+    #TODO   Remove weekends (Create new method)
+    #       Get all the features DIAG, ONC, PRIORITY, AGE, SEX
+    #       Add patient load in system when created
+    #       Add oncologist load
+    #       Try with new dataset
+
+    return (feature, output)
